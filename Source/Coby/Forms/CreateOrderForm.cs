758abd518bbf80e9e6b6c94402bb75af7aa6d5ec
Кирилл Сафонов <frehzzzy@gmail.com>
2022-04-22 19:27:58 +0500

    Количество уже созданных заказов на форме создания заказа

--- Source/Coby/Forms/CreateOrderForm.cs
+++ Source/Coby/Forms/CreateOrderForm.cs
@@ -14,12 +14,15 @@ public partial class CreateOrderForm : MaterialForm
 {
     public IClient Client { get; }
 
+    public List<Order> Orders { get; }
+
     public Credentials Credentials { get; }
 
     public CreateOrderForm(IClient client, Credentials credentials)
     {
         InitializeComponent();
         Client = client;
+        Orders = client.GetByCacheOperation.Order.GetOrders();
         Credentials = credentials;
         _ = FormHelper.CreateMaterialSkinManager(this);
         FormHelper.SetFullScreen(this);
@@ -29,16 +32,21 @@ public partial class CreateOrderForm : MaterialForm
     private void UpdateTableLayoutPanel(IEnumerable<Table> source)
     {
         CreateOrderLayoutPanel.Controls.Clear();
+        IEnumerable<Order> orderOnTable = default;
 
         List<MaterialButton> buttons = new();
         foreach (var item in source)
         {
             MaterialButton button = new();
             button.Click += (sender, e) => CreateOrder(sender, e, item.Id);
-            button.Text = $"Стол #{item.TableNumber}";
+            button.Text = $"Table #{item.TableNumber}";
             button.Tag = item.TableNumber;
             button.Dock = DockStyle.Fill;
             buttons.Add(button);
+
+            orderOnTable = Orders.Where(x => x.Table.Id.Equals(item.Id));
+            if (orderOnTable.Count() > 0)
+                button.Text += string.Concat(Environment.NewLine, $"Count of orders on this table: {orderOnTable.Count()}");
         }
 
         CreateOrderLayoutPanel.Controls.AddRange(buttons.OrderBy(x => int.Parse(x.Tag.ToString())).ToArray());
